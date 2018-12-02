6b0bf75f53f81362821885e980ea15b6775b1504
Youve <minny.molly@gmx.com>
2018-12-02 14:03:06 +0100

    Documented the function for the sake of my future self who will have forgotten how polar coordinates work again.

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,6 +3,19 @@
 import cmath, readline
 
 def mandelbrot(z):
+    '''Just take a point called z in the complex plane.
+    Let z1 be z0 squared plus z
+    and z2 is z1 squared plus z
+    and z3 is z2 squared pluz z
+    and so on
+    if the series of z's will always stay close to z and never trend away
+    that point is in the Mandelbrot set -- Jonathan Coulton
+    
+    Convert the complex number to polar coordinates. The r coordinate gives
+    the radius from 0. If that stays less than 2 for 1000 tries, it's probably
+    in the Mandelbrot set. If it ever gets bigger than 2, then squaring it will make it
+    rapidly approach infinity and is definitely not in the Mandelbrot set."
+    '''
     newz = z**2 + z
     bailout = 1000
     while cmath.polar(newz)[0] < 2 and bailout > 0:
